75ebef891351a7b781dd85f98c2deca01c9287b6
Olive <supattra@giztix.com>
2018-12-22 20:01:37 +0700

    add action

---
+++ assets/js/canvas-action.js
@@ -0,0 +1,45 @@
+var CanvasAction = {
+    init: function() {
+      this.bindEvents();
+      this.postData("/frameCategory/").done(function (data) {
+        var obj = CanvasAction.JsonParse(data);
+        console.log(obj);
+    });
+    },
+    bindEvents: function() {   
+
+    },
+    postData: function(url, data) {
+        if(data){
+            var data = JSON.stringify(data);
+            this.ajax = $.ajax({url: url, type: 'post',data: {data} });
+        }else{
+            this.ajax = $.ajax({url: url, type: 'post'});
+        }
+        return this.ajax;
+    },
+    JsonParse: function(data) {
+        if(data == "<script>location.reload();</script>"){
+            window.location = "/login";
+            return;
+        }
+        if (/^[\],:{}\s]*$/.test(data.replace(/\\["\\\/bfnrtu]/g, '@').
+        replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+        replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+            var obj = JSON.parse(data);
+        }else{
+            var obj = {
+                "en" : data,
+                "th" : data
+            };
+        }
+        /*
+        if(obj.error == "session EXP"){
+            window.location = "/login";
+            return;
+        }
+        */
+        return obj;
+    },
+};
+CanvasAction.init();
